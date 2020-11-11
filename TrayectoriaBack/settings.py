3af2a5df0389c8ab8e1c09741a2a0d8338dfa6c5
Viviana Rojo <193291@ids.upchiapas.edu.mx>
2020-11-10 22:34:46 -0600

    Hide Credenrials BD

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -105,35 +105,17 @@ WSGI_APPLICATION = 'TrayectoriaBack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#DATABASES = {
-#   'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#       'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#} 
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'trayectoriadb',
-#        'USER': 'postgres',
-#        'PASSWORD': 'vivi12Rojo29SQL_BD',
-#        'HOST': '127.0.0.1',
-#        'PORT': '5432'
-#    }
-#}
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd1m17sf6vj3013',
-        'USER': 'kqaqbkjcsoeoiy',
-        'PASSWORD': '209ebb03b2661e526b147c6e887ed12502b3bfefca3cb127559b9243643eb28e',
-        'HOST': 'ec2-34-231-56-78.compute-1.amazonaws.com',
-        'PORT': '5432'
+        'ENGINE': config('ENGINE'),
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT')
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
